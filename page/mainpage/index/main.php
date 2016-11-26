cd96cd6540dedf632d23f49ef2c91783be25df40
phphuc <nothingzzzpp@gmail.com>
2016-11-26 16:13:01 +0700

    New Update Comment Select Item in DB

--- page/mainpage/index/main.php
+++ page/mainpage/index/main.php
@@ -49,7 +49,51 @@ include_once "/home/s3568988/public_html/setting/config.php";
 
                 <div class="row">
 <?php
-$numberOfItem = 30;
+// SQL Select Item
+/* From This 
+$a = mysqli_query($connect5, "SELECT * FROM cc");
+$b = mysqli_num_rows($a);
+if ($b > 0){
+	while ($c = mysqli_fetch_array($a)){
+		$vl[0] = $a['Column GI do'];
+		$vl[1] = $a['Column GI do'];
+		$vl[2] = $a['Column GI do'];
+		$vl[3] = $a['Column GI do'];
+		
+		$thisdisplay = '<div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img src="http://placehold.it/320x150" alt="">
+                            <div class="caption">
+                                <h4 class="pull-right">'.$vl[0].'</h4>
+                                <h4><a href="#">First Product</a>
+                                </h4>
+                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com '.$vl[1].'</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">15 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+';
+	
+		$finaldisplay += $thisdisplay;
+	}
+}else{
+	$finaldisplay = "There is no item to get";	
+}
+
+echo $finaldisplay;	
+
+To This*/
+$numberOfItem = 1000;
+
 $display = '
 <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
