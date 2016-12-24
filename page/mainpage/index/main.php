a4043e4869ee322c58d5572ceae0191f3da9a7e1
phphuc <Phuc@Phams-MacBook-Pro-3.local>
2016-12-24 16:05:14 +0700

    list item

--- page/mainpage/index/main.php
+++ page/mainpage/index/main.php
@@ -51,24 +51,24 @@ include_once "/home/s3568988/public_html/setting/config.php";
                 <div class="row">
 <?php
 // SQL Select Item
-/* From This 
-$a = mysqli_query($connect5, "SELECT * FROM cc");
+// From This 
+$a = mysqli_query($connect5, "SELECT * FROM item");
 $b = mysqli_num_rows($a);
 if ($b > 0){
 	while ($c = mysqli_fetch_array($a)){
-		$vl[0] = $a['Column GI do'];
-		$vl[1] = $a['Column GI do'];
-		$vl[2] = $a['Column GI do'];
+		$vl[0] = $a['I_Name'];
+		$vl[1] = $a['I_Price'];
+		$vl[2] = $a['I_Img'];
 		$vl[3] = $a['Column GI do'];
 		
 		$thisdisplay = '<div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="http://placehold.it/320x150" alt="">
+                            <img src="'.$vl[2].'" alt="">
                             <div class="caption">
-                                <h4 class="pull-right">'.$vl[0].'</h4>
-                                <h4><a href="#">First Product</a>
+                                <h4 class="pull-right">'.$vl[1].'</h4>
+                                <h4><a href="#">'.$vl[0].'</a>
                                 </h4>
-                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com '.$vl[1].'</a>.</p>
+                                
                             </div>
                             <div class="ratings">
                                 <p class="pull-right">15 reviews</p>
@@ -92,7 +92,9 @@ if ($b > 0){
 
 echo $finaldisplay;	
 
-To This*/
+//To This
+
+/*
 $numberOfItem = 6;
 
 $display = '
@@ -103,7 +105,7 @@ $display = '
                                 <h4 class="pull-right">$24.99</h4>
                                 <h4><a href="#">First Product</a>
                                 </h4>
-                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com '.$vl[1].'</a>.</p>
                             </div>
                             <div class="ratings">
                                 <p class="pull-right">15 reviews</p>
@@ -124,7 +126,7 @@ $display = '
 for($x = 0; $x < $numberOfItem; $x++){
 echo $display; 	
 }
-
+*/
 
 ?>
                     
