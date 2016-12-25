0def7ae11108d4e5139c4de4e896fb4434699787
phphuc <Phuc@Phams-MacBook-Pro-3.local>
2016-12-25 23:17:19 +0700

    change var

--- page/mainpage/index/main.php
+++ page/mainpage/index/main.php
@@ -53,21 +53,21 @@ include_once "/home/s3568988/public_html/setting/mysql_config.php";
 <?php
 // SQL Select Item
 // From This 
-$a = mysqli_query($connect5, "SELECT * FROM item");
-$b = mysqli_num_rows($a);
-if ($b > 0){
-	while ($c = mysqli_fetch_array($a)){
-		$vl[0] = $a['I_Name'];
-		$vl[1] = $a['I_Price'];
-		$vl[2] = $a['I_Img'];
-		$vl[3] = $a['Column GI do'];
+$connectDB = mysqli_query($connect5, "SELECT * FROM item");
+$itemrow = mysqli_num_rows($connectDB);
+if ($itemrow > 0){
+	while ($detail = mysqli_fetch_array($connectDB)){
+		$value[0] = $detail['I_Name'];
+		$value[1] = $detail['I_Price'];
+		$value[2] = $detail['I_Img'];
+		$value[3] = $detail['Column GI do'];
 		
 		$thisdisplay = '<div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <img src="'.$vl[2].'" alt="">
+                            <img src="'.$value[2].'" alt="">
                             <div class="caption">
-                               <h4 class="pull-right">'.$vl[1].'</h4>
-                                <h4><a href="#">'.$vl[0].'</a>
+                               <h4 class="pull-right">'.$value[1].'</h4>
+                                <h4><a href="#">'.$value[0].'</a>
                                 </h4>
                                 
                             </div>
