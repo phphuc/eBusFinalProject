5761bb240bae9a56f7ac9aec2c2b6861d966ea55
KhaiTo97 <toquangkhai2411997@gmail.com>
2016-12-30 09:52:38 +0700

    cart by Khải
    hello

--- page/mainpage/shopping_cart/cart_template.php
+++ page/mainpage/shopping_cart/cart_template.php
@@ -7,15 +7,39 @@ include_once "/home/s3568988/public_html/setting/config.php";
 <?php
 $getItemFromDb = mysqli_query($connect5, "SELECT * FROM item");
 if(mysqli_num_rows($getItemFromDb)>0){
+ //$data = array();
+ if(!isset($_SESSION['cart']) or empty($_SESSION['cart'])){
+	echo "<div class='alert alert-danger'>Nothing in cart</div>"; 
+	 }
+	 else{
+		 $total=0;
+		 foreach($_SESSION['cart'] as $value){
+			 $total += $value['quantity'];
+			 
+			 }
+		echo "<div class='alert alert-danger'>You have: $total item.</div>";	  
+		 }
  while($row = mysqli_fetch_assoc($getItemFromDb)){
 	echo "<ul>";
 	echo "<li><h3>$row[I_Name]</h3></li>";
 	echo "<li>$row[I_Img]</li>";
 	echo "<li>$row[I_Price]</li>";
-	echo "<li><a href='#' style='color:red;font-weight:bold'>Add to cart</a></li>";
+	echo "<li><a href='insert.php?id=$row[I_ID]' style='color:red;font-weight:bold'>Add to cart</a></li>";
 	echo "</ul>";
 	echo "<hr>"; 
+	//$data[] = $row;
  }
 }
+ /*echo "<pre>";
+ print_r($data);
+ echo "</pre>";*/
+ /*$newarr = array();
+ foreach($data as $value){
+	$newarr[$value['I_ID']] = $value;
+ }
+ /*echo "<pre>";
+ print_r($newarr);
+ echo "</pre>";*/
+ 
 ?>
 </div>
---
+++ page/mainpage/shopping_cart/insert.php
@@ -0,0 +1,34 @@
+<?php
+include_once "/home/s3568988/public_html/setting/config.php";
+?>
+
+<?php
+//echo $_GET['id'];
+
+$getItemFromDb = mysqli_query($connect5, "SELECT * FROM item");
+if(mysqli_num_rows($getItemFromDb)>0){
+ $data = array();
+ while($row = mysqli_fetch_assoc($getItemFromDb)){
+	$data[$row['I_ID']] = $row;
+ }
+}
+ $id = $_GET['id'];
+ if(!isset($_SESSION['cart']) or empty($_SESSION['cart'])){
+	$data[$id]['quantity']=1;
+	$_SESSION['cart'][$id] = $data[$id];
+	 
+	 } else{
+     if(array_key_exists($id,$_SESSION['cart'])){
+		 $_SESSION['cart'][$id]['quantity']+=1;
+		 
+		 } else{
+		   $data[$id]['quantity']=1;
+	       $_SESSION['cart'][$id] = $data[$id];
+		   echo "<pre>";
+		   print_r($_SESSION['cart']);
+			 }
+		 } 
+header("location:index.php");
+ 
+ 
+?>
