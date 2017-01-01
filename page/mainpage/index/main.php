aab98735c1eafebc94061de7c5674902cc131e1e
phphuc <Phuc@Phams-MacBook-Pro-3.local>
2017-01-01 15:51:53 +0700

    stuck in display modal of products

--- page/mainpage/index/main.php
+++ page/mainpage/index/main.php
@@ -10,9 +10,18 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
             <div class="col-md-3">
                 <p class="lead">Shop Name</p>
                 <div class="list-group">
+                    <?php
+						$getCate=mysqli_query($connect5, "select * from type");
+						while ($rowCate=mysqli_fetch_array($getCate)) {
+							$CateId=$rowCate['T_Name'];	
+						echo '<a href="#" class="list-group-item">'.$CateId.'</a>';
+						}
+					?>
+                    <!--
                     <a href="#" class="list-group-item">Keyboard</a>
                     <a href="<?php echo $url_s."page/mainpage/items/cart_template.php"?>" class="list-group-item">Keycap</a>
                     <a href="#" class="list-group-item">Others</a>
+                    -->
                     
                 </div>
             </div>
@@ -60,33 +69,58 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
 										<img src="'.$item['I_Img'].'" alt="" class="img-responsive" style="max-height:200px;vertical-align:middle;display:inline-block;height:100%;">
 										</div>
 										<div class="caption">
-										   <h4 data-toggle="modal" data-target="#showItem"><a href="#">'.$item['I_Name'].'</a></h4>
+										   <h4><a data-toggle="modal" href="#showItem'.$item['I_ID'].'">'.$item['I_Name'].'</a></h4>
 										   <h4 class="pull-right col-md-4 col-sm-4 col-xs-6">'.$item['I_Price'].'</h4>
 										</div>	
 										
 									
 											
-					</div>
-								</div>											
+									</div>
+								</div>
+							<!-- Modal -->
+  <div class="modal fade" id="showItem'.$item['I_ID'].'" role="dialog">
+    <div class="modal-dialog modal-lg">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        	<div class="modal-header">
+          			<button type="button" class="close" data-dismiss="modal">&times;</button>
+          			<h4 class="modal-title">'.$item['I_Name'].'</h4>
+        	</div>
+       <div class="modal-body">
+          	<div style="max-width:600px;max-height:400px;margin: 0 auto;">
+          		<img class="img-responsive" src="'.$item['I_Img'].'"/>
+          	</div>
+          	<div>
+          		<p>'.$item['I_Description'].'</p>
+           </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+	 <!--End modal -->											
 
 										
 										
 							';
 						}
-		
+						
 					?>
-                </div>
+                </div> 
 
             </div>
 
         </div>
 
 </div>
-<!-- Modal -->
+
+	<!-- Modal
   <div class="modal fade" id="showItem" role="dialog">
     <div class="modal-dialog modal-lg">
     
-      <!-- Modal content-->
+      <!-- Modal content
       <div class="modal-content">
         	<div class="modal-header">
           			<button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -105,8 +139,7 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
         </div>
       </div>
     </div>
-	 <!--End modal -->
-						
+	 End modal -->					
 
 <!-- Just For Send -->
 
