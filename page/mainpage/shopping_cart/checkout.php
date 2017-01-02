4b56a3b4266e8aa75c806a3ec86a839df1a5c5a7
KhaiTo97 <toquangkhai2411997@gmail.com>
2017-01-03 00:13:02 +0700

    checkout for guest (shipping cost)

--- page/mainpage/shopping_cart/checkout.php
+++ page/mainpage/shopping_cart/checkout.php
@@ -2,7 +2,35 @@
 include_once "/home/s3568988/public_html/setting/config.php";
 ?>
 
-<?php
-
-
-?>
+<div class="container">
+  <h1>Checkout</h1>
+  <form class="col-md-6 col-md-offset-3 col-sm-12 col-lg-4 col-lg-offset-4" action="<?php echo $url_s."page/mainpage/shopping_cart/checkoutcontroller.php"?>" method="post">
+  
+    <div class="form-group">
+      <label for="email">Email address:</label>
+      <input type="email" class="form-control" id="email" name="email">
+    </div>
+   <div class="form-group">
+      <label for="Cname">Full Name:</label>
+      <input type="text" class="form-control" id="Cname" name="Cname">
+    </div>
+    
+    <div class="form-group">
+      <label for="phone">Phone number:</label>
+      <input type="text" class="form-control" id="phone" name="phone">
+    </div>
+    <div class="form-group">
+      <label for="address">Address:</label>
+      <input type="text" class="form-control" id="address" name="address">
+    </div>
+    
+    <!-- check box remember me
+    <div class="checkbox">
+      <label>
+        <input type="checkbox">
+        Remember me</label>
+    </div>
+    -->
+    <button type="submit" class="btn btn-default"  >Submit</button>
+  </form>
+</div>
---
+++ page/mainpage/shopping_cart/checkoutcontroller.php
@@ -0,0 +1,20 @@
+<?php
+include_once "/home/s3568988/public_html/setting/config.php";
+?>
+
+<?php
+$email = $_POST["email"];
+$name = $_POST["Cname"];
+$phone = $_POST["phone"];
+$address = $_POST["address"];
+$date = date('Y-m-d H:i:s');
+
+if (!$updateProfile = mysqli_query($connect5, "INSERT INTO `order` (`email`, `name`, `phone`, `shippingAddress`, `date`) VALUES ('".$email."', '".$name."', '".$phone."', '".$address."','".$date."')")){
+	echo mysqli_error($connect5);	
+}  else {
+echo "You are successfully placed your order<br>";
+header('Refresh: 3;url=http://mekong1.rmit.edu.vn/~s3568988/page/mainpage/shopping_cart');
+};
+
+
+?>
