7dc2d8c40670978fcfa5f272eb99266771233330
phphuc <Phuc@Phams-MacBook-Pro-3.local>
2017-01-02 23:57:32 +0700

    admin

--- page/mainpage/admin/editItem.php
+++ page/mainpage/admin/editItem.php
@@ -7,10 +7,10 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
 	if(isset($_GET['editItem'])){
 	$getEditItem=mysqli_query($connect5,"select * from item where I_ID = '".$_GET['editItem']."'");
 	};
-?>
-
-<h1>Insert new product</h1>
-<form class="col-md-12" action="<?php echo $url_s."page/mainpage/admin/insertItem.php"?>" method="post">
+	while ($resultEditItem=mysqli_fetch_array($getEditItem)){
+	echo '
+<h1>Edit product(not finsih)</h1>
+<form class="col-md-12" id="updateForm" action="<?php echo $url_s."page/mainpage/admin/insertItem.php"?>" method="post">
 	<div class="form-group">
       <label for="iName">Name:</label>
       <input type="text" class="form-control" id="iName" name="iName">
@@ -19,13 +19,13 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
       <label for="iType">Type:</label>
       <select class="form-control" id="iType" name="iType">
       		<option>Select a type</option>
-      <?php
-	  $getType=mysqli_query($connect5,"select * from type");
+      
+	  $getType=mysqli_query($connect5,"select * from type");';
 	  while ($rowType=mysqli_fetch_array($getType)){
 			echo '<option value="'.$rowType['T_ID'].'">'.$rowType['T_Name'].'</option>';  
 	  };
-	  ?>
-      	</select>
+	  echo
+      	'</select>
     </div>
     <div class="form-group">
       <label for="iImg">Image:</label>
@@ -39,4 +39,32 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
       <label for="iDes">Description:</label>
       <textarea class="form-control" rows="5" id="iDes" name="iDes">
     </div>
-</form>
+    <button type="submit" name="updateItem">Submit</button>
+    <button type="reset">Reset</button>
+</form>';
+	};
+?>
+<script>
+$('#updateForm').submit(function(e){
+    var text = $('#iDes').val();
+	var cleanText = text.replace(/(<([^>]+)>)/ig,"");
+    //alert(cleanText);
+    $('#iDes').val(cleanText);
+    e.preventDefault();
+});
+</script>
+};
+?>
+<?php
+	if(isset($_POST['updateItem'])){
+		$iImage=$_FILES['iImg']['name'];
+		$iImageTmp=$FILES['iImg']['tmp_name'];
+		move_uploaded_file($iImageTmp,"".$url_s."page/mainpage/items/img/$iImage");
+	
+		$insertToDB=mysqli_query($connect5, "update item set I_Name='".$_POST['iName']."',I_Type='".$_POST['iType']."',I_Img='".$_POST['iImg']."',I_Price='".$_POST['iPrice']."',I_Description='".$_POST['iDes']."' where I_ID=''");
+		if($insertToDB) {
+			echo "<script>alert('New product has been inserted!')</script>";
+			echo "<script>window.open('https://mekong1.rmit.edu.vn/~s3568988/page/mainpage/admin/index.php?insertItem','_self')</script>";
+			}
+		}
+?>
--- page/mainpage/admin/insertItem.php
+++ page/mainpage/admin/insertItem.php
@@ -5,14 +5,14 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
 
 
 <h1>Insert new product</h1>
-<form class="col-md-12" action="<?php echo $url_s."page/mainpage/admin/insertItem.php"?>" method="post">
+<form class="col-md-12" id="insertForm" action="<?php echo $url_s."page/mainpage/admin/index.php?insertItem.php"?>" method="post">
 	<div class="form-group">
       <label for="iName">Name:</label>
-      <input type="text" class="form-control" id="iName" name="iName">
+      <input type="text" class="form-control" id="iName" name="iName" required>
     </div>
     <div class="form-group">
       <label for="iType">Type:</label>
-      <select class="form-control" id="iType" name="iType">
+      <select class="form-control" id="iType" name="iType" required>
       		<option>Select a type</option>
       <?php
 	  $getType=mysqli_query($connect5,"select * from type");
@@ -24,16 +24,38 @@ include_once "/home/s3568988/public_html/page/mainpage/index/controller/getItemC
     </div>
     <div class="form-group">
       <label for="iImg">Image:</label>
-      <input type="file" class="form-control" id="iImg" name="iImg">
+      <input type="file" class="form-control" id="iImg" name="iImg" required>
     </div>
     <div class="form-group">
       <label for="iPrice">Price:</label>
-      <input type="text" class="form-control" id="iPrice" name="iPrice">
+      <input type="text" class="form-control" id="iPrice" name="iPrice" required>
     </div>
     <div class="form-group">
       <label for="iDes">Description:</label>
       <textarea class="form-control" rows="5" id="iDes" name="iDes"></textarea>
     </div>
-    <button type="submit">Submit</button>
+    <button type="submit" name="submitItem">Submit</button>
     <button type="reset">Reset</button>
 </form>
+<script>
+$('#insertForm').submit(function(e){
+    var text = $('#iDes').val();
+	var cleanText = text.replace(/(<([^>]+)>)/ig,"");
+    //alert(cleanText);
+    $('#iDes').val(cleanText);
+    e.preventDefault();
+});
+</script>
+<?php
+	if(isset($_POST['submitItem'])){
+		$iImage=$_FILES['iImg']['name'];
+		$iImageTmp=$FILES['iImg']['tmp_name'];
+		move_uploaded_file($iImageTmp,"".$url_s."page/mainpage/items/img/$iImage");
+	
+		$insertToDB=mysqli_query($connect5, "insert into item ('I_Name','I_Type','I_Img','I_Price','I_Description') values ('".$_POST['iName']."','".$_POST['iType']."','".$_POST['iImg']."','".$_POST['iPrice']."','".$_POST['iDes']."')");
+		if($insertToDB) {
+			echo "<script>alert('New product has been inserted!')</script>";
+			echo "<script>window.open('https://mekong1.rmit.edu.vn/~s3568988/page/mainpage/admin/index.php?insertItem','_self')</script>";
+			}
+		}
+?>
